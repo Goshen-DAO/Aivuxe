9e6cd41cf3b9a5226568440d43dbc377525dc579
dzypherit <31358319+dzypherit@users.noreply.github.com>
2024-02-07 18:43:23 +0800

    fixing meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import {
 import { useAddress } from "@thirdweb-dev/react";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
+import Head from 'next/head';
 
 
 const Home: React.FC = () => {
@@ -29,6 +30,44 @@ const Home: React.FC = () => {
       minH="100vh"
       width="100%"
     >
+         <Head>
+        {/* HTML Meta Tags */}
+        <title>Aivuxe | Permissionless & Open Source NFT Marketplace</title>
+        <meta
+          name="description"
+          content="Aivuxe is a groundbreaking project, an open-source and permissionless NFT marketplace that stands out by seamlessly supporting EVM chains. Offering a liberating space for creators and collectors alike, Aivuxe empowers individuals to engage with NFTs on their terms. Dive into the world of limitless possibilities and creative freedom with Aivuxe."
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://aivuxe.goshendao.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Aivuxe | Permissionless & Open Source NFT Marketplace" />
+        <meta
+          property="og:description"
+          content="Aivuxe is a groundbreaking project, an open-source and permissionless NFT marketplace that stands out by seamlessly supporting EVM chains. Offering a liberating space for creators and collectors alike, Aivuxe empowers individuals to engage with NFTs on their terms. Dive into the world of limitless possibilities and creative freedom with Aivuxe."
+        />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/documents/afa32639-e47d-4db9-8c85-f41605d87fee.png?token=62NNzmR4OB0BsmJj5HBxZaGjxShdl8_CEH2TvjvSl6g&height=682&width=1200&expires=33243301356"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:domain" content="aivuxe.goshendao.com" />
+        <meta property="twitter:url" content="https://aivuxe.goshendao.com/" />
+        <meta
+          name="twitter:title"
+          content="Aivuxe | Permissionless & Open Source NFT Marketplace"
+        />
+        <meta
+          name="twitter:description"
+          content="Aivuxe is a groundbreaking project, an open-source and permissionless NFT marketplace that stands out by seamlessly supporting EVM chains. Offering a liberating space for creators and collectors alike, Aivuxe empowers individuals to engage with NFTs on their terms. Dive into the world of limitless possibilities and creative freedom with Aivuxe."
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/documents/afa32639-e47d-4db9-8c85-f41605d87fee.png?token=62NNzmR4OB0BsmJj5HBxZaGjxShdl8_CEH2TvjvSl6g&height=682&width=1200&expires=33243301356"
+        />
+      </Head>
       {!address ? (
         <Container maxW="100%">
           <Flex
